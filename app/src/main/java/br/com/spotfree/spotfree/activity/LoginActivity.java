6eddc5420e4d374dbf72f2f41dd7c850cfa05bb1
Renato Yuzo <renatoyuzo93@gmail.com>
2018-03-15 20:48:54 -0400

    Criando tela Login.

---
+++ app/src/main/java/br/com/spotfree/spotfree/activity/LoginActivity.java
@@ -0,0 +1,36 @@
+package br.com.spotfree.spotfree.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import br.com.spotfree.spotfree.R;
+
+/**
+ * Created by re_ja on 15/03/2018.
+ */
+
+public class LoginActivity extends Activity {
+
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.login);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Login!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
